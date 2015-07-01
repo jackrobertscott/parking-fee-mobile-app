a64b897cdc42d950020dc0aa5548282a3b663a4a
Jack Scott <mast.j.scott@gmail.com>
2015-07-01 11:07:50 +0800

    add side menu icons and no data list items

--- app/scripts/sidemenu/sidemenu.controller.js
+++ app/scripts/sidemenu/sidemenu.controller.js
@@ -27,37 +27,58 @@
       menu.addItem({
         label: 'Register',
         direction: 'register',
-        maxRole: 'guest'
+        maxRole: 'guest',
+        data: {
+          icon: 'ion-person-add'
+        }
       });
       menu.addItem({
         label: 'Login',
         direction: 'login',
-        maxRole: 'guest'
+        maxRole: 'guest',
+        data: {
+          icon: 'ion-log-in'
+        }
       });
       menu.addItem({
         label: 'Park',
         direction: 'app.sessionStart',
-        minRole: 'user'
+        minRole: 'user',
+        data: {
+          icon: 'ion-map'
+        }
       });
       menu.addItem({
         label: 'Profile',
         direction: 'app.userSettings',
-        minRole: 'user'
+        minRole: 'user',
+        data: {
+          icon: 'ion-person'
+        }
       });
       menu.addItem({
         label: 'Vehicles',
         direction: 'app.vehicleUser',
-        minRole: 'user'
+        minRole: 'user',
+        data: {
+          icon: 'ion-android-car'
+        }
       });
       menu.addItem({
         label: 'Sessions',
         direction: 'app.sessionUser',
-        minRole: 'user'
+        minRole: 'user',
+        data: {
+          icon: 'ion-calendar'
+        }
       });
       menu.addItem({
         label: 'Logout',
         direction: 'app.userLogout',
-        minRole: 'user'
+        minRole: 'user',
+        data: {
+          icon: 'ion-log-out'
+        }
       });
       return menu.getItems();
     }
